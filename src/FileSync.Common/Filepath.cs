e6c4221d8655b5260e84079e159830aff9fd3648
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-07-29 20:25:31 -0400

    Serialize Filepath as string

--- src/FileSync.Common/Filepath.cs
+++ src/FileSync.Common/Filepath.cs
@@ -1,26 +1,11 @@
-﻿using System;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-
-using Recore;
+﻿using Recore;
 
 namespace FileSync.Common
 {
-    [JsonConverter(typeof(FilepathJsonConverter))]
+    [OfJson(typeof(Filepath), typeof(string))]
     public sealed class Filepath : Of<string>
     {
+        public Filepath() { }
         public Filepath(string value) => Value = value;
-    }
-
-    internal class FilepathJsonConverter : JsonConverter<Filepath>
-    {
-        public override Filepath Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        {
-            var value = reader.GetString();
-            return new Filepath(value);
-        }
-
-        public override void Write(Utf8JsonWriter writer, Filepath value, JsonSerializerOptions options)
-           => writer.WriteStringValue(value.Value);
     }
 }
