c7a80e3e3e23c869098df24628e0c679490f8454
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-08-15 16:29:30 -0400

    Use Microsoft.Extensions.DependencyInjection in the client

--- src/FileSync.Client/Program.cs
+++ src/FileSync.Client/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Recore;
 
 using FileSync.Client.UI;
@@ -11,24 +12,40 @@ namespace FileSync.Client
 {
     static class Program
     {
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            var currentDirectory = new SystemFilepath(Directory.GetCurrentDirectory());
+
+            // Use a single HTTP client for connection pooling
+            // (not that it really matters in this application)
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new AbsoluteUri("http://localhost:5000/")
+            };
+
+            services
+                .AddSingleton<IFileStoreFactory>(new FileStoreFactory(currentDirectory))
+                .AddSingleton<IFileHasher, FileHasher>()
+                .AddSingleton<ITextView>(new ConsoleView { IsVerbose = true })
+                .AddSingleton<IFileServiceApi>(new FileServiceHttpClient(httpClient))
+                .AddSingleton<SyncClient>();
+        }
+
+        private static ServiceProvider BuildServiceProvider()
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+
+            return services.BuildServiceProvider();
+        }
+
         static async Task<int> Main()
         {
             try
             {
-                var currentDirectory = new SystemFilepath(Directory.GetCurrentDirectory());
+                var serviceProvider = BuildServiceProvider();
 
-                // Use a single HTTP client for connection pooling
-                // (not that it really matters in this application)
-                using var httpClient = new HttpClient
-                {
-                    BaseAddress = new AbsoluteUri("http://localhost:5000/")
-                };
-
-                var syncClient = new SyncClient(
-                    view: new ConsoleView { IsVerbose = true },
-                    fileStoreFactory: new FileStoreFactory(currentDirectory),
-                    fileHasher: new FileHasher(),
-                    fileService: new FileServiceHttpClient(httpClient));
+                var syncClient = serviceProvider.GetService<SyncClient>();
 
                 await syncClient.RunAsync();
                 return 0;
